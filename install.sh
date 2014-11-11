5b621db989158284924360369b240cd403d2fd1e
Yigong Wang <yigwang@cs.stonybrook.edu>
2014-11-10 23:10:33 -0500

    Alter the prev^Cext tab

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+echo "Backing up orignal files..."
+mv ~/.vimrc ~/.vimrc_backup
+mv ~/.vim ~/.vim_backup
+
+printf 'Installing files...\n'
+cp -r .vimrc .vim ~/
+
+printf 'Updating bundles...\n'
+cd ~/.vim/bundle
+for i in `ls`; do
+	cd "$i"
+		printf '[Bundle]:%s: ' $i
+		git pull
+	cd ..
+done
+
+while true; do
+    read -p "Do you wish to keep original config?[y/n]" yn
+    case $yn in
+        [Yy]* ) exit; break;;
+        [Nn]* ) rm -rf ~/.vimrc_backup ~/.vim_backup; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+printf 'Installation finished.\n'
